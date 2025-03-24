a363c9c2ea1a70899218b46ee2d0c6cba3d36a1a
Wigfloo <baepsae510@gmail.com>
2025-03-23 23:40:01 -0500

    Integracion con Streamlit

--- app/fetch_athlete.py
+++ app/fetch_athlete.py
@@ -1,6 +1,6 @@
 import requests
 import config
-
+import streamlit as st
 def get_athlete_data():
     """Obtiene y muestra la información del atleta"""
     url = "https://www.strava.com/api/v3/athlete"
@@ -9,11 +9,11 @@ def get_athlete_data():
     response = requests.get(url, headers=headers)
     
     if response.status_code == 200:
-        print("✅ Datos del atleta:")
-        print(response.json())
+        st.write("✅ Datos del atleta:")
+        st.write(response.json())
     else:
-        print("❌ Error al obtener datos:", response.status_code)
-        print(response.json())
+        st.write("❌ Error al obtener datos:", response.status_code)
+        st.write(response.json())
 
 # Llamar la función solo cuando se necesite
 # get_athlete_data()
