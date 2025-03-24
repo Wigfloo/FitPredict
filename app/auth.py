27c7b0a0f193b7c55f77cad95db5e67ce014260c
Wigfloo <baepsae510@gmail.com>
2025-03-24 18:07:14 -0500

    haciendo que cualquier usuario pueda entrar al aplicativoweb.

--- app/auth.py
+++ app/auth.py
@@ -1,5 +1,8 @@
 import requests
 import config
+import streamlit as st
+
+CODE = ''
 
 def refresh_access_token():
     """Renueva el Access Token usando el Refresh Token"""
@@ -20,5 +23,36 @@ def refresh_access_token():
         print("❌ Error al renovar token:", response.status_code)
         print(response.json())
 
-# Ejecutar para obtener el token actualizado
-refresh_access_token()
+def getAccessToken():
+    uri_exchange = f"{config.URL_API}/oauth/token"
+    params = {
+        "client_id": config.CLIENT_ID,
+        "client_secret": config.CLIENT_SECRET,
+        "code": CODE,
+        "grant_type": "authorization_code"
+    }
+    response = requests.post(uri_exchange, data=params)
+    if response.status_code == 200:
+        getResponse = response.json()
+        refresh_token = getResponse['refresh_token']
+        athlete = getResponse['athlete']
+        config.REFRESH_TOKEN = refresh_token
+        print("✅ Refresh Token:", refresh_token)
+        print("✅ Athlete:", athlete)
+        #guardar session en el navegador de token y la informacion del usuario del 
+        return True
+    else:
+        print("❌ Error al obtener token:", response.status_code)
+        print(response.json())
+        print(f'Mi codigo: {CODE}')
+        return False
+
+
+def checkAuthorization():
+    #Validar primero que exista una seccion de navegador del usuario donde deben estar guardado el token del usuario y el json con la info del usuario, para no volver a pedir esta informacion
+    query_params = st.query_params
+    if "code" in query_params:
+        global CODE
+        CODE = query_params['code']
+        return getAccessToken()
+    return False
--- app/main.py
+++ app/main.py
@@ -2,20 +2,25 @@ import auth
 import fetch_athlete
 import fetch_activities
 import streamlit as st
+import acces_code as acc
 
+#crear ruta de navegacion ,menu 
 def main():
-    st.write("\n🏃‍♂️ Bienvenido a Strava CLI!")
-    st.write("1. Ver mi perfil")
-    st.write("2. Ver mis últimas actividades")
-    opcion = st.text_input("Selecciona una opción: ", key="opcion")
+    # Validamos que el usuario tenga una sesion activa
+    if auth.checkAuthorization():
+        st.write("\n🏃‍♂️ Bienvenido a Strava CLI!")
+        st.write("1. Ver mi perfil")
+        st.write("2. Ver mis últimas actividades")
+        opcion = st.text_input("Selecciona una opción: ", key="opcion")
 
-    if opcion == "1":
-        st.write(fetch_athlete.get_athlete_data())
-    elif opcion == "2":
-       st.write( fetch_activities.get_activities())
+        if opcion == "1":
+            st.write(fetch_athlete.get_athlete_data())
+        elif opcion == "2":
+            st.write( fetch_activities.get_activities())
+        else:
+            st.write("❌ Opción inválida.")
     else:
-        st.write("❌ Opción inválida.")
+        acc.getAuthorization()
 
 if __name__ == "__main__":
-    auth.refresh_access_token()  # Asegurar que el token esté actualizado
     main()
