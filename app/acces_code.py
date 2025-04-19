2e8820eec11c373e22c9574ae88a588409cededd
Wigfloo <baepsae510@gmail.com>
2025-04-19 00:56:16 -0500

    try

--- app/acces_code.py
+++ app/acces_code.py
@@ -2,7 +2,7 @@ import streamlit as st
 import config
 
 def getAuthorization():
-    REDIRECT_URI = "http://localhost:8501"  # O la URL pública de tu app
+    REDIRECT_URI = "https://fitpredict.streamlit.app/"  # O la URL pública de tu app
 
     auth_url = f"https://www.strava.com/oauth/authorize?client_id={config.CLIENT_ID}&response_type=code&redirect_uri={REDIRECT_URI}&approval_prompt=force&scope=read,activity:read"
 
