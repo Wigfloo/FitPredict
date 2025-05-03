f79abc75a9f191c0cb6d2ee978dce2e553529249
Wigfloo <baepsae510@gmail.com>
2025-05-02 19:36:34 -0500

    Add
    Añadicion de la base de datos firebase

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ import fetch_athlete
 import fetch_activities
 import acces_code as acc
 
+
 def main():
     st.set_page_config(page_title="FitPredict - Strava Sync", page_icon="🏃")
     st.title("🏃 Bienvenido a FitPredict")
@@ -12,15 +13,15 @@ def main():
     # 1. Verificamos si ya existe un código de autorización en la URL (usuario ya autorizó)
     if auth.checkAuthorization():
         st.success("✅ Conexión con Strava exitosa!")
-        
+
         # 2. Mostramos el menú de opciones
-        opcion = st.selectbox("¿Qué deseas hacer?", ["Selecciona una opción", "Ver mi perfil", "Ver mis últimas actividades"])
+        opcion = st.selectbox("¿Qué deseas hacer?", ["Selecciona una opción", "Ver mi perfil", "Ver mis últimas actividades y subirlas a Firebase"])
 
         if opcion == "Ver mi perfil":
             fetch_athlete.get_athlete_data()
 
-        elif opcion == "Ver mis últimas actividades":
-            fetch_activities.get_activities()
+        elif opcion == "Ver mis últimas actividades y subirlas a Firebase":
+            fetch_activities.get_activities() # Llama a la función modificada que sube a Firebase
 
     else:
         # 3. Si no hay código, mostramos el link para autorizar Strava
@@ -29,3 +30,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
