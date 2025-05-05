a0fe86efd8afd304089c741bff1afcac8008fc30
Wigfloo <baepsae510@gmail.com>
2025-05-04 23:11:42 -0500

    add

--- app/acces_code.py
+++ app/acces_code.py
@@ -3,7 +3,7 @@ import config
 
 def getAuthorization():
     
-    REDIRECT_URI = "http://localhost:8501"  # URL donde va a llegar la autorización de strava
+    REDIRECT_URI = "http://localhost:8502"  # URL donde va a llegar la autorización de strava
 
     auth_url = f"https://www.strava.com/oauth/authorize?client_id={config.CLIENT_ID}&response_type=code&redirect_uri={REDIRECT_URI}&approval_prompt=force&scope=read,activity:read"
 
